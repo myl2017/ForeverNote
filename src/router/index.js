fe4323f447b6539be9db0b05f02b86ac7269e5df
myl920716 <706695013@qq.com>
2018-07-28 16:57:18 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -11,9 +11,9 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+        path: '/login',
+        name: 'Login',
+        component: Login
     },
     {
       path: '/login',
