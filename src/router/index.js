e18573bbb60b77a069d6072347aae69c81f2f52b
myl920716 <706695013@qq.com>
2018-07-28 21:16:41 +0800

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Login from '@/components/Login'
-import NotebookList from '@/components/NotebookList'
-import NoteDetail from '@/components/NoteDetail'
-import TashDetail from '@/components/TashDetail'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-        path: '/login',
-        name: 'Login',
-        component: Login
+        path: '/',
+        alias: '/notebooks',
+        component: () => import('@/components/NotebookList.vue')
     },
     {
       path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/notebooks',
-      name: 'NotebookList',
-      component: NotebookList
+      component: () => import('@/components/Login.vue')
     },
     {
       path: '/note',
-      name: 'NoteDetail',
-      component: NoteDetail
+      component: () => import('@/components/NoteDetail.vue')
     },
     {
       path: '/trash',
-      name: 'TrashDetail',
-      component: TashDetail
+      component: () => import('@/components/TashDetail.vue')
     }
   ]
 })
